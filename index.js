3f9478225b19038e35087db4f760f6bdfa7c49e1
) <sumin@192-168-1-117.tpgi.com.au>
2021-06-28 20:50:13 +1000

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,20 +2,41 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+//application/json
+app.use(express.json());
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://suminkim:zpzprjf4@boilerplate.e98cp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("로또당첨되세요"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listeninig on port ${port}!`));
